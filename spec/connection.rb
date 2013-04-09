ef09ed4135da408e740cfe3465c9686e1f20bd67
Karel Malbroukou <karel.malbroukou@rea-group.com>
2013-04-09 17:08:10 +1000

    Finished login test

--- spec/connection.rb
+++ spec/connection.rb
@@ -13,25 +13,19 @@ describe Netscaler::Connection do
   end
 
   describe "#login" do
-    it "should succeed" do
+    it do
       stub_request( :post, "http://10.0.0.1/nitro/v1/config").
         with(:body => {"object"=>"{\"login\":{\"username\":\"user\",\"password\":\"pass\"}}"},
           :headers => {'Accept'=>'application/json', 'Content-Type'=>'application/x-www-form-urlencoded'}).
         to_return({ :body => '{"errorcode": 0, "message": "Done", "sessionid": "##CCD41760A2B71E88E029BC33F00E9C24704E71821EB86BD9A3AD2E5005C5" }', :status => 200, :headers => {'Content-Type' => 'application/json' }})
 
-      session_id = @connection.login
-
-      if session_id == "##CCD41760A2B71E88E029BC33F00E9C24704E71821EB86BD9A3AD2E5005C5"
-        return true
-      else
-        return false
-      end
+      @connection.login.should be_eql "##CCD41760A2B71E88E029BC33F00E9C24704E71821EB86BD9A3AD2E5005C5"
     end
   end
 
   context "when logged in" do
   end
 
-  context "when not logged in" do
+  context "when logged out" do
   end
 end
