2ea10cbdf3f0f91500ba53fada44f479330a4dd0
Karel Malbroukou <karel.malbroukou@rea-group.com>
2013-04-09 17:09:06 +1000

    Added post / login methods

--- lib/netscaler/connection.rb
+++ lib/netscaler/connection.rb
@@ -1,7 +1,10 @@
-module Netscaler
-  attr_reader :hostname
+require 'rest-client'
+require 'json'
 
+module Netscaler
   class Connection
+    attr_reader :hostname, :session_id
+
     def initialize(options = {})
       raise ArgumentError unless options.is_a? Hash
       raise ArgumentError unless options.count.eql? 3
@@ -10,6 +13,31 @@ module Netscaler
       @username = options["username"]
       @password = options["password"]
       @hostname = options["hostname"]
+
+      @base_url = "http://#{@hostname}/nitro/v1"
+      @content_type = "application/x-www-form-urlencoded"
+      @session_id = nil
+    end
+    
+    def login
+      payload = { "login" => { "username" => @username, "password" => @password }}
+
+      response = post payload
+      @session_id = JSON.parse(response)["sessionid"]
+   end
+
+   private
+   
+   def post(payload = {})
+      begin
+        response = RestClient.post @base_url + "/config", "object=#{payload.to_json}", :content_type => @content_type, :accept => "json"
+      rescue => e
+        puts e.message
+        puts e.backtrace.inspect
+        return false
+      end
+
+      return response
     end
   end
 end
