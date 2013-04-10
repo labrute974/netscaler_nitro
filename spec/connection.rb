8ef2de4da0b1426bf0a2868074ac07a7dec63505
Karel Malbroukou <karel.malbroukou@rea-group.com>
2013-04-10 13:37:39 +1000

    Added logged in / logged out contexts and relative methods

--- spec/connection.rb
+++ spec/connection.rb
@@ -4,7 +4,7 @@ require "webmock/rspec"
 describe Netscaler::Connection do
   before(:all) do
     @connection = Netscaler::Connection.new({"username" => "user", "password" => "pass", "hostname" => "10.0.0.1"})
-  end
+ end
 
   describe "#new" do
     it "takes a hash as parameter" do
@@ -12,20 +12,31 @@ describe Netscaler::Connection do
     end
   end
 
-  describe "#login" do
-    it do
-      stub_request( :post, "http://10.0.0.1/nitro/v1/config").
+  context "when logged out" do
+    it { @connection.should_not be_logged_in }
+
+    describe "#login" do
+      it "should succeed" do
+        stub_request( :post, "http://10.0.0.1/nitro/v1/config").
         with(:body => {"object"=>"{\"login\":{\"username\":\"user\",\"password\":\"pass\"}}"},
           :headers => {'Accept'=>'application/json', 'Content-Type'=>'application/x-www-form-urlencoded'}).
         to_return({ :body => '{"errorcode": 0, "message": "Done", "sessionid": "##CCD41760A2B71E88E029BC33F00E9C24704E71821EB86BD9A3AD2E5005C5" }', :status => 200, :headers => {'Content-Type' => 'application/json' }})
+ 
+        @connection.login.should be_eql "##CCD41760A2B71E88E029BC33F00E9C24704E71821EB86BD9A3AD2E5005C5"
+      end
+    end
 
-      @connection.login.should be_eql "##CCD41760A2B71E88E029BC33F00E9C24704E71821EB86BD9A3AD2E5005C5"
+    describe "#logout" do
     end
   end
 
   context "when logged in" do
-  end
-
-  context "when logged out" do
+    it { @connection.should be_logged_in }
+    describe "#login" do
+      it { @connection.login.should be_eql "##CCD41760A2B71E88E029BC33F00E9C24704E71821EB86BD9A3AD2E5005C5"}
+    end
+    
+    describe "#logout" do
+    end
   end
 end
