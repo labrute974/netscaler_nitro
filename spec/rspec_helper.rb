8c415aeef4518609f4ec16886e0f9a42b3857e40
Karel Malbroukou <karel.malbroukou@rea-group.com>
2013-04-11 15:21:41 +1000

    Moved webmock includes in the rspec helper

--- spec/rspec_helper.rb
+++ spec/rspec_helper.rb
@@ -2,3 +2,6 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'netscaler'
+require "webmock/rspec"
+
+include WebMock::API
