b2cc3fdea8fbff9c10191d7a291f6842b1ff868e
Karel Malbroukou <karel.malbroukou@rea-group.com>
2013-04-12 16:10:52 +1000

    added rspec for Netscaler::Server enable / disable methods

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -19,6 +19,20 @@ class WebHTTPMock
         with(:headers => {'Accept'=>'application/json', 'Content-Type'=>'application/x-www-form-urlencoded', 'Cookie' => "sessionid=##CCD41760A2B71E88E029BC33F00E9C24704E71821EB86BD9A3AD2E5005C5"}).
         to_return({ :body => '{"errorcode": 0, "message": "Done", "server": [{"name": "srv1", "ipaddress": "1.1.1.1", "state": "ENABLED"}] }', :status => 200, :headers => {'Content-Type' => 'application/json' }})
   end
+  
+  def self.enable
+    stub_request(:post, "http://10.0.0.1/nitro/v1/config").
+      with(:body => {"object"=>"{\"params\":\"enable\",\"server\":{\"name\":\"srv3\"}}"},
+       :headers => {'Accept'=>'application/json', 'Content-Type'=>'application/x-www-form-urlencoded', 'Cookie'=>'sessionid=##CCD41760A2B71E88E029BC33F00E9C24704E71821EB86BD9A3AD2E5005C5'}).
+       to_return({ :status => 200, :body => '{"errorcode": 0, "message": "Done"}', :headers => {'Content-Type' => 'application/json'}})
+  end
+  
+  def self.disable
+    stub_request(:post, "http://10.0.0.1/nitro/v1/config").
+      with(:body => {"object"=>"{\"params\":\"disable\",\"server\":{\"name\":\"srv3\"}}"},
+       :headers => {'Accept'=>'application/json', 'Content-Type'=>'application/x-www-form-urlencoded', 'Cookie'=>'sessionid=##CCD41760A2B71E88E029BC33F00E9C24704E71821EB86BD9A3AD2E5005C5'}).
+       to_return({ :status => 200, :body => '{"errorcode": 0, "message": "Done"}', :headers => {'Content-Type' => 'application/json'}})
+  end
 end
 
 describe Netscaler::Server do
@@ -27,10 +41,7 @@ describe Netscaler::Server do
     WebHTTPMock.login
     connection.login
   end
-  
-  describe "#update" do
-  end
-  
+
   describe "#self.get_all" do
     specify do
       WebHTTPMock.get_all
@@ -39,22 +50,63 @@ describe Netscaler::Server do
   end
 
   describe "#self.find_by_name" do
-    specify do
-      WebHTTPMock.find_by_name
-      Netscaler::Server.find_by_name(connection, "srv1").should be_an_instance_of Netscaler::Server
-    end
+   specify do
+     WebHTTPMock.find_by_name
+     Netscaler::Server.find_by_name(connection, "srv1").should be_an_instance_of Netscaler::Server
+   end
   end
-  
-  describe "#self.find_by_ip" do
-    specify do
-      WebHTTPMock.get_all
+
+ describe "#self.find_by_ip" do
+   specify do  
+     WebHTTPMock.get_all
       Netscaler::Server.find_by_ip(connection, "1.1.1.1").should be_an_instance_of Netscaler::Server
-    end
+   end
   end
   
   describe "#self.add" do
   end
   
-  describe "#self.delete" do
+  context "when Netscaler::Server instanciated" do
+    context "when enabled" do
+      let(:server) {Netscaler::Server.new(connection, "srv3", {"ipaddress" => "1.1.1.1", "state" => "ENABLED"})}
+      describe "#enable!" do
+        it { server.enable!.should be true }
+      end
+  
+      describe "#disable!" do
+        specify do
+          WebHTTPMock.disable
+          server.disable!.should be true
+        end
+      end
+
+      describe "#enable?" do
+        it {server.enable?.should be true}
+      end
+    end
+    
+    context "when disabled" do
+      let(:server) {Netscaler::Server.new(connection, "srv3", {"ipaddress" => "1.1.1.1", "state" => "DISABLED"})}
+      describe "#enable!" do
+        specify do
+          WebHTTPMock.enable
+          server.enable!.should be true
+        end
+      end
+  
+      describe "#disable!" do
+        it { server.disable!.should be true }
+      end
+      
+      describe "#enable?" do
+        it {server.enable?.should_not be true}
+      end
+    end
+    
+    describe "#update" do
+    end
+  
+    describe "#self.delete" do
+    end
   end
 end
