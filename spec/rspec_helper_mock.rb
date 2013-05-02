e662381a650b5340e26a45f39e7ac3c6dc5b5c5c
Karel Malbroukou <karel.malbroukou@rea-group.com>
2013-05-02 12:27:18 +1000

    Added stub_it method for WebHTTPMock to simplify each Mock subclasses

--- spec/rspec_helper_mock.rb
+++ spec/rspec_helper_mock.rb
@@ -7,6 +7,15 @@ require "webmock/rspec"
 include WebMock::API
 
 class WebHTTPMock
+  @@headers = {'Accept'=>'application/json', 'Content-Type'=>'application/x-www-form-urlencoded', 'Cookie'=>'sessionid=##CCD41760A2B71E88E029BC33F00E9C24704E71821EB86BD9A3AD2E5005C5'}
+  
+  def self.stub_it(request)
+      stub_request(:post, "http://10.0.0.1/nitro/v1/config").
+      with(:body => { "object" => request.to_json},
+       :headers => @@headers).
+       to_return({ :status => 200, :body => '{"errorcode": 0, "message": "Done"}', :headers => {'Content-Type' => 'application/json'}})
+  end
+  
   def self.login
     request = { "login" => { "username" => "user", "password" => "pass" }}
     stub_request( :post, "http://10.0.0.1/nitro/v1/config").
@@ -17,40 +26,29 @@ class WebHTTPMock
 
   def self.enable(name)
     request = { "params" => { "action" => "enable" }, get_type => { get_nsname_key => name }}
-    stub_request(:post, "http://10.0.0.1/nitro/v1/config").
-      with(:body => { "object" => request.to_json},
-       :headers => {'Accept'=>'application/json', 'Content-Type'=>'application/x-www-form-urlencoded', 'Cookie'=>'sessionid=##CCD41760A2B71E88E029BC33F00E9C24704E71821EB86BD9A3AD2E5005C5'}).
-       to_return({ :status => 200, :body => '{"errorcode": 0, "message": "Done"}', :headers => {'Content-Type' => 'application/json'}})
+    stub_it request
   end
   
   def self.disable(name)
     request = { "params" => { "action" => "disable" }, get_type => { get_nsname_key => name }}
-    
-    stub_request(:post, "http://10.0.0.1/nitro/v1/config").
-      with(:body => { "object" => request.to_json },
-        :headers => {'Accept'=>'application/json', 'Content-Type'=>'application/x-www-form-urlencoded', 'Cookie'=>'sessionid=##CCD41760A2B71E88E029BC33F00E9C24704E71821EB86BD9A3AD2E5005C5'}).
-      to_return({ :status => 200, :body => '{"errorcode": 0, "message": "Done"}', :headers => {'Content-Type' => 'application/json'}})
+    stub_it request
   end
   
   def self.rename(name,newname)
-    request = { "params" => { "action" => "rename" }, get_type => { get_nsname_key => name, "new" + get_nsname_key => newname }}
-    stub_request(:post, "http://10.0.0.1/nitro/v1/config").
-      with(:body => { "object" => request.to_json },
-        :headers => {'Accept'=>'application/json', 'Content-Type'=>'application/x-www-form-urlencoded', 'Cookie'=>'sessionid=##CCD41760A2B71E88E029BC33F00E9C24704E71821EB86BD9A3AD2E5005C5'}).
-       to_return({ :status => 200, :body => '{"errorcode": 0, "message": "Done"}', :headers => {'Content-Type' => 'application/json'}})
+    request = { "params" => { "action" => "rename" }, get_type => { "new" + get_nsname_key => newname, get_nsname_key => name }}
+    stub_it request
   end
 
   def self.delete(name)
     stub_request( :delete, "http://10.0.0.1/nitro/v1/config/#{get_type}/#{name}").
-      with(:headers => {'Accept'=>'application/json', 'Content-Type'=>'application/x-www-form-urlencoded', 'Cookie' => "sessionid=##CCD41760A2B71E88E029BC33F00E9C24704E71821EB86BD9A3AD2E5005C5"}).
+      with(:headers => @@headers).
       to_return({ :body => '{"errorcode": 0, "message": "Done"}', :status => 200, :headers => {'Content-Type' => 'application/json' }})
   end
   
   def self.logout
     stub_request( :post, "http://10.0.0.1/nitro/v1/config").
       with(:body => {"object"=>"{\"logout\":{}}"},
-        :headers => {'Accept'=>'application/json', 'Content-Type'=>'application/x-www-form-urlencoded', 'Cookie' => 'sessionid=##CCD41760A2B71E88E029BC33F00E9C24704E71821EB86BD9A3AD2E5005C5'}).
+        :headers => @@headers).
       to_return({ :body => '{"errorcode": 0, "message": "Bye!"}', :status => 200, :headers => {'Content-Type' => 'application/json'}})
   end
-  
 end
