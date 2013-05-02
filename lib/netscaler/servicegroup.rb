12f68dc5cde75ffdaa8cb1332e5cf51059925cbf
Karel Malbroukou <karel.malbroukou@rea-group.com>
2013-05-02 13:32:57 +1000

    Added bind / unbind and list_servers methods to Netscaler::ServiceGroup

--- lib/netscaler/servicegroup.rb
+++ lib/netscaler/servicegroup.rb
@@ -24,10 +24,45 @@ module Netscaler
       @params["state"] = "ENABLED" unless @params.has_key? "state"
     end
 
+    def bind(options)
+      raise ArgumentError, "argument must be an Array" unless options.is_a? Array
+      
+      required_attrs = ["servername", "port"]
+      avail_attrs = ["servername", "port", "serverid", "hashid", "weight"]
+      
+      options.each do |option|
+        required_attrs.each {|attr| raise ArgumentError, "the Hash doesn't have the required keys (arg:1) : should have #{required_attrs.to_s}" unless option.include? attr }
+        option.keys.each {|k| raise ArgumentError, "unknown key (arg:1) : #{k}" unless avail_attrs.include? k}
+      end
+      
+      send_action("bind", options)
+    end
+    
+    def unbind(options)
+      raise ArgumentError, "argument must be an Array" unless options.is_a? Array
+      
+      attrs = ["servername", "port"]
+      options.each do |option|
+        attrs.each {|attr| raise ArgumentError, "the Hash doesn't have the required keys (arg:1) : should have #{attrs.to_s}" unless option.include? attr }
+      end
+      
+      send_action("unbind", options)
+    end
+
+    def list_servers
+      result = @nitro.get("servicegroup_servicegroupmember_binding/#{@name}")["servicegroup_servicegroupmember_binding"]
+      
+      if result
+        result.each {|server| server.delete @nsname_key }
+      end
+      
+      result
+    end
+    
     def self.get_options
       ["servicetype", "maxclient", "cip", "cipHeader", "usip", "downstateflush", "sc", "sp", "srvtimeout", "clttimeout", "useproxyport", "tcpb", "comment", "state", "cmp", "maxreq", "appflowlog" ]
     end
-
+    
     def self.get_type
       "servicegroup"
     end
@@ -35,5 +70,6 @@ module Netscaler
     def self.get_nsname_key
       "servicegroupname"
     end
+    
   end
 end
